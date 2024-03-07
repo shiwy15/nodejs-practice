bbcbe71813f6adc3a0d7dd241e6b1d65e0f8ddb5
shiwy15 <shiwy15@gmail.com>
2024-03-07 21:51:17 +0900

    2024-3-7 async-await 마무리ㅊ

--- asynchronous/async-await-test.js
+++ asynchronous/async-await-test.js
@@ -1,6 +1,7 @@
-// setTimeout으로 10까지 차례로 세기
+/** setTimeout으로 10까지 차례로 세기 */ 
 
 function waitOneSecond(msg) {
+    // setTimeout은 타이머ID가 반환되어 비동기 처리되지 않으므로, Promise로 감싸서 반환
     return new Promise((resolve, _) => {
         setTimeout(() => resolve(`${msg}`), 1000);
     });
@@ -15,3 +16,29 @@ async function countOneToTen() {
 }
 
 countOneToTen();
+
+/** async-await 사용하지 않은 경우 */
+// // 1: 1~10초 대기중 차례로 -> 완료
+// function countOneToTen() {
+//     for (let i = 1; i <= 10; i++) {
+//         setTimeout(() => {
+//             console.log(`${i}초 대기 중...`);
+//             if (i === 10) {
+//                 console.log("완료");
+//             }
+//         }, i * 1000);
+//     }
+// }
+// // 2: 완료 -> 1~10초 대기중 한꺼번에
+// // for문이 순회하며 i=1~10인 1초짜리 타이머가 한꺼번에 설정됨
+// // 이 사이에 countOneToTen이 실행 종료되며 "완료"가 먼저 찍힌 후,
+// // 10개의 타이머가 동시에 종료되며 한꺼번에 대기중 문구가 10개 찍힘
+// function countOneToTen() {
+//     for (let i = 1; i <= 10; i++) {
+//         setTimeout(() => {
+//             console.log(`${i}초 대기 중...`);
+//         }, 1000);
+//     }
+//    console.log("완료");
+// }
+// countOneToTen();
--- asynchronous/movieTOP20-async-await.js
+++ asynchronous/movieTOP20-async-await.js
@@ -1,41 +1,31 @@
 const axios = require("axios");
-const url = "https://raw.githubusercontent.com/wapj/musthavenodejs/main/movieinfo.json";
 
-axios
-    .get(url)   // get 요청
-    .then((result) => {
-        // 상태코드가 200이 아닐 경우 에러
-        if (result.status != 200) {
-            throw new Error("요청에 실패했습니다!");
-        }
-        // result.data가 있으면 결과 반환
-        if (result.data) {
-            return result.data;
-        }
-        // result.data가 없는 경우 에러
-        throw new Error("데이터가 없습니다.");
-    })
-    .then((data) => {
-        // 데이터 크기가 0이면 에러
+async function getTop20Movies() {
+    const url = "https://raw.githubusercontent.com/wapj/musthavenodejs/main/movieinfo.json";
+    try {
+        // 1. get 요청(네트워크에서 받아오므로 await로 기다림)
+        const result = await axios.get(url);
+        
+        // 2. 데이터에 결과값이 없을 때 예외처리 
+        const {data} = result;
         if (!data.articleList || data.articleList.size == 0) {
             throw new Error("데이터가 없습니다.");
         }
-        // 영화 리스트 반환
-        return data.articleList;
-    })
-    .then((articles) => {
-        // 리스트를 제목과 순위로 분리
-        return articles.map((article, idx) => {
+
+        // 3. 데이터에서 영화제목과 순위 정보 추출
+        const movieInfos = data.articleList.map((article, idx) => {
             return { title: article.title, rank: idx + 1 };
         });
-    })
-    .then((results) => {
-        // 영화 정보 출력
-        for (let movieInfo of results) {
+
+        // 4. 데이터 출력
+        for (let movieInfo of movieInfos) {
             console.log(`[${movieInfo.rank}위] ${movieInfo.title}`);
         }
-    })
-    .catch((err) => {   // 중간에 에러 발생 시 여기서 처리 
-        console.log("<<에러 발생>>");
-        console.error(err);
-    });
+    } catch (err) {
+        throw new Error(err);
+        // console.log("<<에러 발생>>");
+        // console.error(err);
+    }
+}
+
+getTop20Movies();
